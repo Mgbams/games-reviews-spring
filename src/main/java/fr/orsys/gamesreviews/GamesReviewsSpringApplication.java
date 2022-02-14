f634f592f1062b3f47aa74e738d3d30518964cca
MrN0 <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>
2022-02-14 16:35:25 +0100

    Init commit. Created spring boot project

---
+++ src/main/java/fr/orsys/gamesreviews/GamesReviewsSpringApplication.java
@@ -0,0 +1,13 @@
+package fr.orsys.gamesreviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesReviewsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesReviewsSpringApplication.class, args);
+	}
+
+}
